6a4ce3920ff89ab9d18156a9a92265cca8969501
hieunugent <hieunugent@gmail.com>
2021-05-28 18:36:41 -0700

    Updates

---
+++ precache-manifest.628666e1f9bcd31dea81d4f2e97e16e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3469788ac78d9274cb277988a038ce5",
+    "url": "/armandos/index.html"
+  },
+  {
+    "revision": "d9fd2b36cc91344ea3a5",
+    "url": "/armandos/static/css/main.8cbddc3d.chunk.css"
+  },
+  {
+    "revision": "6cf0ee389ef3430f9f79",
+    "url": "/armandos/static/js/2.fa0ffaac.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/armandos/static/js/2.fa0ffaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fd2b36cc91344ea3a5",
+    "url": "/armandos/static/js/main.c2f1b4f3.chunk.js"
+  },
+  {
+    "revision": "7dd008276c91ba7c578e",
+    "url": "/armandos/static/js/runtime-main.b0390eed.js"
+  },
+  {
+    "revision": "3c684f2557c9696d01939b3d3aa7b888",
+    "url": "/armandos/static/media/page1.3c684f25.jpg"
+  },
+  {
+    "revision": "6bb86df5ca482ba627a8aae9423f6714",
+    "url": "/armandos/static/media/page2.6bb86df5.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/armandos/precache-manifest.628666e1f9bcd31dea81d4f2e97e16e7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/armandos/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
